ccbee608467a9b9fbb33682fd1c9fde0e21c9462
Semmwa <bakshaksemmwa@gmail.com>
2023-02-21 10:59:03 +0100

    merge

--- controllers/attendanceController.js
+++
@@ -1,8 +0,0 @@
-
-exports.showAttendancePage = ( req, res) => {
-    res.render('attendance')
-}
-
-exports.verifyPresentAtd = ( req, res) => {
-    console.log('You are present')
-}
--- models/db.js
+++ models/db.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose')
-const dotenv = require("dotenv")
-dotenv.config()
 
-mongoose.set("strictQuery", false)
-mongoose.connect( process.env.DB_URI)
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.DB_URI)
     .then(() => {
-            console.log("db connected")
+            console.log("db connected".bgMagenta.underline.bold)
         })
     .catch((error) => {
         console.log(error)
-        // console.log("error not connected")
     })
-    // .bgMagenta.underline.bold
